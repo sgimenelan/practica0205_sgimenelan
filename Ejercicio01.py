16d28869fe5b100bb460a98caba6a613216e0961
sgimenelan <sgimenelan@educacion.navarra.es>
2024-11-08 08:56:29 +0100

    Update Ejercicio01.py

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -2,6 +2,11 @@
 # y muestre por pantalla el saludo ¡hola <nombre>!.
 def bienvenido(nombre):
     """"función a la que se le pase una cadena <nombre>
-    y muestre por pantalla el saludo ¡hola <nombre>!."""
+    y muestre por pantalla el saludo ¡hola <nombre>!.
+    Parámetros:
+...   - nombre: Un nombre de una persona
+... Salida:
+      - ¡hola <nombre>!.  
+"""
     print("¡Hola", nombre + "!")
 bienvenido(input("Dime tu nombre: "))
