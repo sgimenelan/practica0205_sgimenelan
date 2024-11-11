d73502367a763969023d783d8a4435496e7c170a
sgimenelan <sgimenelan@educacion.navarra.es>
2024-11-11 10:43:41 +0100

    Update Ejercicio04.py

--- Ejercicio04.py
+++ Ejercicio04.py
@@ -9,6 +9,13 @@ while numero != "no hay mas":
 numeros.pop(len(numeros) - 1)
 numeros = list(map(float, numeros))
 def media(n):
+    """función que reciba una muestra de números 
+    en una lista y devuelva su media
+    Parámetros:
+... - n = lista de numeros
+... Salida:
+      - media de esos numeros
+    """
     return statistics.mean(n)
 resultado = media(numeros)
 print(round(resultado,2))
